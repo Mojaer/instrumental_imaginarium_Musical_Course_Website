053a02e16b8e6b4b2a910442f1ab194401506058
Mojaer Ahmed <mojaermohi@gmail.com>
2023-06-08 00:25:00 +0600

    error page is added

---
+++ src/Shared/404_Not_Found/Error_Page.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import error from '../../assets/error_page.jpg'
+
+
+const Error_Page = () => {
+    return (
+        <div className="mx-10 text-center mt-10">
+            <Link className="btn btn-error text-bold text-white" to='/'>Back to Homepage</Link>
+            <img className="mx-auto" src={error} alt="" />
+
+        </div>
+    );
+};
+
+export default Error_Page;
