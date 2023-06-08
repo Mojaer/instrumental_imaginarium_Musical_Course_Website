c5ea40a5237c9a811f30199d20db292bc5e0bf59
Mojaer Ahmed <mojaermohi@gmail.com>
2023-06-08 10:48:50 +0600

    axios action is commited

---
+++ src/Components/AxiosAction/useAxiosAction.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+const axiosAction = axios.create({
+    baseUrl: 'http://localhost:3000',
+})
+
+const useAxiosAction = () => {
+
+    useEffect(() => {
+        axiosAction.interceptors.request.use((config) => {
+            return config
+        })
+    }, [])
+    axiosAction.interceptors.response.use((response) => {
+        return response
+    })
+
+    return axiosAction
+};
+
+export default useAxiosAction;
--- src/Layout/Dashboard/Dashboard.jsx
+++ src/Layout/Dashboard/Dashboard.jsx
@@ -1,22 +1,29 @@
+import Footer from "../../Shared/Footer/Footer";
+import Header from "../../Shared/Header/Header";
 
 
 const Dashboard = () => {
     return (
-        <div className="drawer lg:drawer-open drawer-end justify-items-end">
-            <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content">
-                {/* Page content here */}
-                <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary">Open drawer</label>
+        <>
+            <Header></Header>
+            <div className="drawer lg:drawer-open drawer-end justify-items-end">
+                <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content">
+                    {/* Page content here */}
+                    <label htmlFor="my-drawer-4" className="drawer-button mt-5 btn btn-primary">Open drawer</label>
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-60 rounded-lg mt-4 h-3/4 bg-base-300 text-base-content">
+                        {/* Sidebar content here */}
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </ul>
+                </div>
             </div>
-            <div className="drawer-side">
-                <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 h-3/4 bg-base-300 text-base-content">
-                    {/* Sidebar content here */}
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
-                </ul>
-            </div>
-        </div>
+            <Footer></Footer>
+        </>
+
     );
 };
 
